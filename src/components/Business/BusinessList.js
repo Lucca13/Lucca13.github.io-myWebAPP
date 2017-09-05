add5f2f7dcf849b992a2635d15ef67c43a9494f3
Zach Case (Cow) <zcase@homeadvisor.com>
2017-09-05 13:41:28 -0600

    Fix react errors

--- src/components/Business/BusinessList.js
+++ src/components/Business/BusinessList.js
@@ -3,14 +3,18 @@ import './Business.css';
 import Business from '../Business/Business';
 
 class BusinessList extends React.Component {
+	constructor(props) {
+		super(props);
+	}
 	render() {
 		return (
 			<div className="BusinessList">
 			  {
 			    this.props.businesses.map(
-			      callBackBusiness(business) {
-			        return <Business business={this.business} />;
-			      });
+			      function(business) {
+			        return <Business business={business} />;
+			      }
+					)
 			  }
 			</div>
 		);
